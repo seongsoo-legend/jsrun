8d2be05e9a51b6705508705ff691ad3254c4d89e
seongsoo-legend <44110310+seongsoo-legend@users.noreply.github.com>
2019-02-13 16:25:28 +0900

    객체까지 진도나감

--- run.js
+++ run.js
@@ -1,13 +1,86 @@
-let colorList = ["푸른", "하얀", "초록", "노랑", "오렌지"];
-let natureList = ["태양", "달", "구름", "하늘", "무지개"];
-let wordList = ["눈물", "웃음", "환생", "행복", "기쁨"];
-let feelList = ["격렬한", "영원한", "평온한", "안락한"]
+// 배열 공부
+// let colorList = ["푸른", "하얀", "초록", "노랑", "오렌지"];
+// let natureList = ["태양", "달", "구름", "하늘", "무지개"];
+// let wordList = ["눈물", "웃음", "환생", "행복", "기쁨"];
+// let feelList = ["격렬한", "영원한", "평온한", "안락한"]
 
-let color = colorList[Math.floor(Math.random() * colorList.length)];
-let nature = natureList[Math.floor(Math.random() * natureList.length)];
-let word = wordList[Math.floor(Math.random() * wordList.length)];
-let feel = feelList[Math.floor(Math.random() * feelList.length)];
+// let color = colorList[Math.floor(Math.random() * colorList.length)];
+// let nature = natureList[Math.floor(Math.random() * natureList.length)];
+// let word = wordList[Math.floor(Math.random() * wordList.length)];
+// let feel = feelList[Math.floor(Math.random() * feelList.length)];
 
-let userName = `${feel} ${color} ${nature}의 ${word}이다!`;
+// let userName = `${feel} ${color} ${nature}의 ${word}이다!`;
 
-console.log(userName);
+// console.log(userName);
+
+// let dog = {
+//   legs: 4,
+//   name: "boo",
+//   color: "white",
+//   bark: "walwal",
+// };
+
+// let cat = {
+//   legs: 4,
+//   name: "baba",
+//   color: "gold",
+//   bark: "nyaong",
+// };
+
+// console.log(dog);
+// console.log(cat);
+
+// let bird = {};
+
+// bird["legs"] = 3;
+// bird["name"] = "blue",
+// bird["color"] = "blue";
+
+// let bird = {};
+// bird.legs = 3;
+// bird.name = "blue";
+// bird.color = "blue";
+
+// let animals = [
+//   { kind: "dog", year: 1 },
+//   { kind: "cat", year: 2 },
+//   { kind: "bird", year: 0},
+// ];
+//종류만 가져올때
+// animals[0]["kind"];
+// animals[1]["year"];
+
+// animals[2].kind;
+
+// let seongsoo = { name: "seongsoo", age: 30, luckyNumbers: [2, 5, 10, 28] };
+// let jisoo = { name: "jisoo", age: 34, luckyNumbers: [0, 1, 7,12]};
+// let boo = { name: "boo", age: 1, luckyNumbers: [3, 6, 8, 9]};
+
+// let family = [seongsoo, jisoo, boo];
+
+// let money = {};
+// money["jisoo"] = 30;
+// money["boo"] = 1;
+
+// let movie = {
+//   "nimo find": {
+//     year: 2003,
+//     learningTime: 100,
+//     actor:["brooks", "dne", "alex"],
+//     type: "DVD"
+//   },
+//   "starWas ep6": {
+//     year: 2001,
+//     learningTime: 140,
+//     actor:["boo", "cene", "kax"],
+//     type: "DVD"
+//   },
+//   "harrypoter": {
+//     year: 1996,
+//     learningTime: 150,
+//     actor:["aio", "edene", "uos"],
+//     type: "blue-ray"
+//   }  
+// }
+
+// let nimo = movie["nimo find"];
